4c19641cb7d7b419ba58708cb527cdd7fcd297b8
ahsan11amimul <amimul.ahsan369@gmail.com>
2023-11-26 21:57:57 +0600

    intial setup and routes added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
